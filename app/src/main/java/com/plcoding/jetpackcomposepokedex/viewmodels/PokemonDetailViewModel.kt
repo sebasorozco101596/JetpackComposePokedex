67850518c81608189ec943c68d5af21c7616ab2a
sebasorozco101596 <sebasorozcob@gmail.com>
2021-05-27 23:41:26 -0400

    Adding the PokemonDetailScreen.kt with the implementation for the image, color, and back arrow.

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/viewmodels/PokemonDetailViewModel.kt
@@ -0,0 +1,19 @@
+package com.plcoding.jetpackcomposepokedex.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.plcoding.jetpackcomposepokedex.model.remote.responses.Pokemon
+import com.plcoding.jetpackcomposepokedex.repository.PokemonRepository
+import com.plcoding.jetpackcomposepokedex.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonDetailViewModel @Inject constructor(
+    private val repository: PokemonRepository
+) : ViewModel() {
+
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon> {
+        return repository.getPokemonInfo(pokemonName)
+    }
+
+}
